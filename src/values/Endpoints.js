d134b6184f8e2feb3533ea967ca6a8581311fbe1
Philip Tuffs <philipmtuffs@gmail.com>
2022-11-13 12:40:54 -0800

    init

--- src/values/Endpoints.js
+++ src/values/Endpoints.js
@@ -5,12 +5,8 @@ export default async function (path) {
 
     let endpoint;
 
-    if (Platform.OS === 'web' && !__DEV__) { // use URL host in production web env
-        endpoint = window.location.host;
-    } else {
-        let config = await (await AppFetch('/config.json', { cache: 'force-cache' })).json();
-        endpoint = config.endpoint;
-    }
+    let config = await (await AppFetch('/config.json', { cache: 'force-cache' })).json();
+    endpoint = config.endpoint;
 
     let paths = {
         dashUpdate: '/_dash-update-component',
